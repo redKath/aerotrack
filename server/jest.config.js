9067999d2d7405a1ef7ddbb03ac29f166ceeeb8b
Kathleen Jogno <jognokath@gmail.com>
2025-11-01 11:49:11 +0800

    update config files

---
+++ server/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
+  testMatch: ['**/tests/**/*.test.(ts|js)'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { /* ts-jest config goes here in Jest */ }],
+  },
+};
